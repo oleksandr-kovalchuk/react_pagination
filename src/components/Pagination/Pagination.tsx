718b948116547ddba93b292638980dc7d4318797
Oleksandr Kovalchuk <kov280396@gmail.com>
2024-12-02 17:21:19 +0200

    add task solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,84 @@
-export const Pagination = () => {};
+import React from 'react';
+
+type Props = {
+  total: number;
+  perPage: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
+};
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  perPage,
+  currentPage,
+  onPageChange,
+}) => {
+  const pageCount = Math.ceil(total / perPage);
+  const pages = Array.from({ length: pageCount }, (_, index) => index + 1);
+
+  const isPrevDisabled = currentPage === 1;
+  const isNextDisabled = currentPage === pageCount;
+
+  const prevPage = currentPage - 1;
+  const nextPage = currentPage + 1;
+
+  const handlePrevClick = () => {
+    if (!isPrevDisabled) {
+      onPageChange(prevPage);
+    }
+  };
+
+  const handleNextClick = () => {
+    if (!isNextDisabled) {
+      onPageChange(nextPage);
+    }
+  };
+
+  const handlePageClick = (page: number) => {
+    onPageChange(page);
+  };
+
+  return (
+    <ul className="pagination">
+      <li
+        className={`page-item ${isPrevDisabled ? 'disabled' : ''}`}
+        onClick={handlePrevClick}
+      >
+        <a
+          className="page-link"
+          href="#prev"
+          data-cy="prevLink"
+          aria-disabled={isPrevDisabled}
+        >
+          «
+        </a>
+      </li>
+
+      {pages.map(page => (
+        <li
+          key={page}
+          className={`page-item ${page === currentPage ? 'active' : ''}`}
+          onClick={() => handlePageClick(page)}
+        >
+          <a className="page-link" href={`#${page}`} data-cy="pageLink">
+            {page}
+          </a>
+        </li>
+      ))}
+
+      <li
+        className={`page-item ${isNextDisabled ? 'disabled' : ''}`}
+        onClick={handleNextClick}
+      >
+        <a
+          className="page-link"
+          href="#next"
+          data-cy="nextLink"
+          aria-disabled={isNextDisabled}
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
